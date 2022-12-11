1bf39e19c62c7ce7ab288d5008b01aed77ae757a
Akshay Jadhav <akshayjadhav2133@gmail.com>
2022-12-11 14:30:09 +0530

    JS-Start

---
+++ Practice/Week-7-JS-Start/Sat.js
@@ -0,0 +1,70 @@
+// // Values
+
+// //primitive Values/ primitive data types-
+// //single value
+// //Number
+// 7
+// 2.5
+
+// //String
+// "Apj"
+// 'apj'
+
+
+// //Boolean
+// true
+// false
+
+
+// //empty values
+// null
+// undefined
+
+// //Non-primitive---More than one val;ue
+// //Array
+// //can have differnt DT
+// [1,2,"apj",true]
+//  // 0  1  2   3 index
+
+// //Object
+
+// {
+//    // key:"value",
+//    // key1:"value2"
+// }
+
+// variables---.place holder for value
+//3-ways to declare variable
+var x=25; //never use var
+
+let y=25;
+const z="akshay";
+
+console.log(y+z);
+//how to run 
+//go to terminal--> node file.js
+
+
+//Template literal
+console.log(`my name is ${z}`);
+
+//history
+//bascic pilers
+//values data types primitivr and non-primitive
+//console.log declaring variable
+//string format---template literal
+
+///Operators
+//assignment  =
+//arithmatic + ,- * /,%
+//comparision ---> == != > < >= <=
+
+
+// == & ===
+
+
+// https://www.youtube.com/@anuragtiwarime
+// Insta - https://www.instagram.com/anuragtiwari.me/
+// Twitter - https://twitter.com/anuragtiwarime/
+// Linkedin - https://www.linkedin.com/in/anuragtiwarime/
+// Github - https://github.com/anuragtiwarime
