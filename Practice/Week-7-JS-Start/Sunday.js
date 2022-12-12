32b590d60d34038db3fe6bbe7398da427df6dbd6
Akshay Jadhav <akshayjadhav2133@gmail.com>
2022-12-12 09:18:58 +0530

    media-query-article-complted

--- Practice/Week-7-JS-Start/Sunday.js
+++ Practice/Week-7-JS-Start/Sunday.js
@@ -1,1 +1,45 @@
 //Sunday JS
+
+
+//switch case
+
+// let user='Admin';
+
+// switch (user) {
+//     case "Admin":
+//         console.log("welcome Amin");
+//         break;
+
+//     case "mentor":
+//         console.log("hi mentior");
+//         break;
+
+//     default:
+//         console.log("i am new");
+// }
+
+//ternairy operator
+
+// condition?True:false
+
+// const now = new Date();
+// console.log(now);
+// console.log(now.toString());
+// console.log(now.getTime());
+// console.log(now.getMinutes());
+
+//Math
+
+// console.log(Math.round(3.4));
+// console.log(Math.round(3.5));
+// console.log(Math.round(3.7));
+
+console.log("Floor of 3.8 is :"+ Math.floor(3.8));
+
+console.log("ceil of 3.8 is :"+ Math.ceil(3.8));
+
+
+
+
+
+
