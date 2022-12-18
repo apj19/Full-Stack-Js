60a9b6eb26ee67ea999629a49606b57fc5f1367c
Akshay Jadhav <akshayjadhav2133@gmail.com>
2022-12-18 07:49:57 +0530

    18-dec

---
+++ Practice/Week-8/saturday.js
@@ -0,0 +1,24 @@
+// console.log("APJ");
+// let n1=8;
+// let array=[1,2,3,4,5,6,7,8,9,9,9,9];
+// array.splice(2,4,11,11,11);
+
+
+// console.log(array.includes(2,4));
+
+
+
+//a.lastindexof()
+// console.log(array.indexOf(9));
+// console.log(array.lastIndexOf(9));
+
+//map will apply to every element
+//pop :- remove last element and array is modified
+//shift:- remove from start
+// let array=[1,2,3,4,5,6,7,8];
+// console.log(array.pop());
+// console.log(array.shift());
+// conso
+let n="qawaearataya";
+console.log(n.split('a'));
+
