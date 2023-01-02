d1495eebb9805e10e7054c78f6a2f91798ff3ae9
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-01-02 10:32:08 +0530

    array-articles

--- Articles/Array_methods/array.js
+++ Articles/Array_methods/array.js
@@ -1,19 +1,104 @@
 console.log("Hi! APJ");
 
-//Array Methods
+// //Array Methods
 
-const array=[8,19,15,9];
-let a=[];
-let arrayofdifferentdatatype=["APJ",8,19,"Akshay",true];
-//console.log(arrayofdifferentdatatype);
+// const array=[8,19,15,9];
 
-//Lenght
-a=5;
-console.log(a);
-// console.log(array.length);
-//Adding/rwemoving element
+// array="apj";
 
- const length= array.push(12,15,21);
+// console.log(array);
 
-//console.log(length);
- console.log(array);
+// let a=10;
+// a="apj";
+// console.log(a);
+// let a=[];
+// let arrayofdifferentdatatype=["APJ",8,19,"Akshay",true];
+// //console.log(arrayofdifferentdatatype);
+
+// //Lenght
+// a=5;
+// console.log(a);
+// // console.log(array.length);
+// //Adding/rwemoving element
+
+//  const length= array.push(12,15,21);
+
+// //console.log(length);
+//  console.log(array);
+
+//Empty Array
+
+// const arr=[];  //This is recommended
+
+// //or
+// const arr1= Array();
+
+// const numbers=[1,2,3,4,0.5,68];
+
+// const tech=["node","angular","react","CSS"];
+
+// //Array can have different data types
+
+// const customer=[
+//     "akshay",
+//     30,
+//     0.25,
+//     true,
+//     {
+//         'country':india,
+//         'city':india
+//     }
+// ]
+
+
+// let js = 'JavaScript'
+// const jsarray= js.split('')
+
+// console.log(jsarray); // ['J', 'a', 'v', 'a','S', 'c', 'r', 'i','p', 't']
+
+// let txt = 'JavaScript is best programming language'
+// const txtarray= txt.split(' ');
+// console.log(txtarray); //[ 'JavaScript', 'is', 'best', 'programming', 'language' ]
+
+
+//Access elements
+
+// const numarray=[10,12,56,14,8,7];
+
+// console.log(numarray[0]);//10
+// console.log(numarray[2]);//56
+// console.log(numarray[4]);//8
+
+
+// const numarray=[10,12,56,14,8,7];
+
+// numarray[0]="modified";
+
+// console.log(numarray); //[ 'modified', 12, 56, 14, 8, 7 ]
+
+// const numarray=[10,12,56,14,8,7];
+// console.log(numarray.length); // 6
+
+// const strarr=["items1","item2","item3"];
+
+// strarr.push("new Item");
+
+// console.log(strarr);//[ 'items1', 'item2', 'item3', 'new Item' ]
+
+// strarr.push("item5",6);
+
+// console.log(strarr); //[ 'items1', 'item2', 'item3', 'new Item', 'item5', 6 ]
+
+// const strarr=["items1","item2","item3"];
+
+// strarr.unshift("at start");
+
+// console.log(strarr);//[ 'at start', 'items1', 'item2', 'item3' ]
+
+
+const strarr=["items1","item2","item3"];
+
+let lastelement=strarr.pop();
+console.log(lastelement);//item3
+console.log(strarr);//[ 'items1', 'item2' ]
+
