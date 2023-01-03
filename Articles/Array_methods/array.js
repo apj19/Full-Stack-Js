68d69b54e84ad78171df734c1db511e31984a23f
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-01-03 10:31:49 +0530

    array-method-article-completed

--- Articles/Array_methods/array.js
+++ Articles/Array_methods/array.js
@@ -126,13 +126,97 @@ console.log("Hi! APJ");
 // run1.splice(3,1,88,88,"new-elements");
 // console.log(run1);// [ 0, 1, 2, 88, 88, 'new-elements', 4, 5, 6 ]
 
- const runs=[0,1,2,3,4,5,6]
+//  const runs=[0,1,2,3,4,5,6]
 
- let copyarray= runs.slice(0,3);
- console.log(copyarray);
- //clone array
- let clonearray=runs.slice();
- console.log(clonearray);//[0, 1, 2, 3,4, 5, 6]
+//  let copyarray= runs.slice(0,3);
+//  console.log(copyarray);
+//  //clone array
+//  let clonearray=runs.slice();
+//  console.log(clonearray);//[0, 1, 2, 3,4, 5, 6]
+
+// const score=[10,12,15,20,10,10,35,40];
+// console.log(score.indexOf(12)); //1 => 12 present at position 1
+// console.log(score.indexOf(99));// -1 => not exist
+// console.log(score.indexOf(10));// 0 => first occourance of that element
+// console.log(score.indexOf(10,1));// 4 => it will start search from index 1
+
+// const numbers = [1, 2, 3, 4, 5, 3, 2]
+
+// console.log(numbers.lastIndexOf(2)) // 6 => last occurance of 2
+// console.log(numbers.lastIndexOf(0)) // -1 => not exist
+// console.log(numbers.lastIndexOf(4)) //  3 => index of 4 
+
+// const numbers=[12,15,18,95,58,42,63];
+// console.log(numbers.includes(12));// true =>exist in array
+// console.log(numbers.includes(99));// false => not exist
+// console.log(numbers.includes(12,1));// false it will start search from index 1
+
+// const numbers=[2,3,4];
+// let num= numbers.map( r => r*5);
+// console.log(num);
+// console.log(numbers);
+
+// let numbers = [1, 3, 5];
+
+// let result = numbers.every( e  => e > 0);
+
+// console.log(result);
+
+// const str=[1,2,3];
+// console.log(str.join(' '));// join by space 1 2 3
+// const str1=["a","p","j"];
+// console.log(str1.join('$'));//join by doller a$p$j
+
+// const odds = [1,3,5];
+// const evens = [2,4,6];
+// // merge odds and evens array
+// const combined = odds.concat(evens);//
+// console.log(combined);// new array=>[ 1, 3, 5, 2, 4, 6 ]
+// console.log(odds);//[ 1, 3, 5 ]
+// console.log(evens);//[ 2, 4, 6 ]
+
+// const circles = [3, 5, 9];
+// // let areas = circles.map(circleArea);
+// let areas= circles.map((r)=> 3*r*r);
+// console.log(areas);//[ 27, 75, 243 ]
+
+// const number =[2,4,3];
+// let iseven= number.map(n => n%2==0);
+// console.log(iseven);//[ true, true, false ]
+
+
+// let num = [10,12,-5,-9,5,-9,52,-8];
+// ///array of positive numbers
+// let positiveNumbers = num.filter(n=> n>0);
+// console.log(positiveNumbers);//[ 10, 12, 5, 52 ]
+
+// let numbers = [1, 3, 5];
+
+// console.log(numbers.some(n => n<2));//false
+
+const stringArray=['z','w','a','d'];
+stringArray.sort();
+console.log(stringArray);//[ 'a', 'd', 'w', 'z' ]
+const numArray=[10,5,12,84,25];
+//if we use only sort 
+numArray.sort();
+console.log(numArray);// [ 10, 12, 25, 5, 84 ]
+//For number array we use compare funtion
+
+numArray.sort((a,b)=>a-b);
+console.log(numArray);// 5, 10, 12, 25, 84 ]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
